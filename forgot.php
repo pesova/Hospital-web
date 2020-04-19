e25f3514315fbcb06f2a52a311271ad61e56da75
pesova <pesova13@gmail.com>
2020-04-19 15:16:59 +0100

    added

--- forgot.php
+++ forgot.php
@@ -21,5 +21,39 @@
     <p>
         <button type="submit">Send Reset Code</button>
     </p>
+
+
+
+
+    <?php
+        //error handlers
+        if(!isset($_GET['signup'])){
+            exit();
+        }
+        else{
+            $signupcheck = $_GET['signup'];
+
+            if($signupcheck == "empty"){
+                echo "<p class='error'>Fill in fields!</p>";
+                exit();
+            }
+            elseif($signupcheck == "CharError"){
+                echo "<p class='error'>You used invalid characters</p>";
+                exit();
+            }
+            elseif($signupcheck == "Email"){
+                echo "<p class='error'>Fill in valid email</p>";
+                exit();
+            }
+            elseif($signupcheck == "success"){
+
+
+                echo "<p class='success'>You have signed up</p>";
+                
+                exit();
+            }
+        }
+
+        ?>
    </form>
     
